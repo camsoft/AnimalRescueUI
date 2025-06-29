76130d3300b7b17e57411867eb313a05200889e2
Chris Cameron <chris.todd.cameron@gmail.com>
2025-06-29 16:07:14 -0700

    Vite config update for Azure Container

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,22 @@ import plugin from '@vitejs/plugin-react';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 63379,
-    }
-})
-
 //export default defineConfig({
-//    plugins: [react()],
+//    plugins: [plugin()],
 //    server: {
-//        watch: {
-//            usePolling: true,
-//        },
-//        host: true, // needed for the Docker Container port mapping to work
-//        strictPort: true,
-//        port: 8080, // you can replace this port with any port
-//        allowedHosts: true,
+//        port: 63379,
 //    }
 //})
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+        host: true, // needed for the Docker Container port mapping to work
+        strictPort: true,
+        port: 8080, // you can replace this port with any port
+        allowedHosts: true,
+    }
+})
